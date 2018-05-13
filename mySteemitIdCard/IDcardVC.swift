404d8777bc566b5c198ff3c0a525c52c276444f9
iamank1t <ankitsingh.t15@gmail.com>
2018-05-13 18:51:40 +0530

    upload image from device camera on your id card feature added

--- mySteemitIdCard/IDcardVC.swift
+++ mySteemitIdCard/IDcardVC.swift
@@ -8,7 +8,9 @@
 
 import UIKit
 import SDWebImage
-class IDcardVC: UIViewController {
+import Photos
+
+class IDcardVC: UIViewController, UINavigationControllerDelegate, UIImagePickerControllerDelegate {
 
     @IBOutlet var mainView: UIView!
     @IBOutlet var profileImage: UIImageView!
@@ -21,6 +23,7 @@ class IDcardVC: UIViewController {
     var finalUserData: [String:AnyObject]?
     var joinDate: String?
     var id : Int?
+    var imagePickerController = UIImagePickerController()
     override func viewDidLoad() {
         super.viewDidLoad()
         self.userNameLabel.text = self.userName
@@ -90,5 +93,144 @@ class IDcardVC: UIViewController {
         }
     }
 
+    @IBAction func addCustomImageButtonClicked(_ sender: Any) {
+        let alert = UIAlertController(title: nil, message: nil, preferredStyle: .actionSheet)
+        let cancelButton = UIAlertAction(title: "Cancel", style: .cancel, handler: {(action: UIAlertAction) -> Void in
+        })
+        
+        let takePhotoButton = UIAlertAction(title: "Take Photo", style: .default, handler: {(action: UIAlertAction) -> Void in
+            let authorizationStatus = AVCaptureDevice.authorizationStatus(for: AVMediaType.video)
+            switch authorizationStatus {
+            case .notDetermined:
+                // permission dialog not yet presented, request authorization
+                AVCaptureDevice.requestAccess(for: AVMediaType.video,
+                                              completionHandler: { (granted:Bool) -> Void in
+                                                if granted {
+                                                    DispatchQueue.main.async(execute: {
+                                                        if UIImagePickerController.isSourceTypeAvailable(.camera)
+                                                        {
+                                                            print("access granted", terminator: "")
+                                                            self.imagePickerController.sourceType = .camera
+                                                            self.imagePickerController.delegate = self
+                                                            self.present(self.imagePickerController, animated: true, completion: {() -> Void in })
+                                                        }
+                                                        else
+                                                        {
+                                                            print("Camera not available")
+                                                        }
+                                                    })
+                                                }
+                                                else {
+                                                    print("access denied", terminator: "")
+                                                }
+                })
+            case .authorized:
+                if UIImagePickerController.isSourceTypeAvailable(.camera) {
+                    self.imagePickerController.sourceType = .camera
+                    self.imagePickerController.delegate = self
+                    self.present(self.imagePickerController, animated: true, completion: {() -> Void in })
+                } else {
+                    print("Camera not available")
+                }
+                break
+            case .denied, .restricted:
+                self.alertToEncourageCameraAccessWhenApplicationStarts()
+            }
+            
+            
+        })
+        
+        let chooseExistingButton = UIAlertAction(title: "Choose Existing", style: .default, handler: { (action: UIAlertAction) -> Void in
+            if PHPhotoLibrary.authorizationStatus() == PHAuthorizationStatus.authorized
+            {
+                UINavigationBar.appearance().titleTextAttributes = [
+                    NSAttributedStringKey.font : UIFont(name: "Ubuntu", size: 16)!,
+                    NSAttributedStringKey.foregroundColor : UIColor.black
+                ]
+                self.imagePickerController.sourceType = .photoLibrary
+                self.imagePickerController.delegate = self
+                self.present(self.imagePickerController, animated: true, completion: {() -> Void in
+                })
+                
+            }
+            else if PHPhotoLibrary.authorizationStatus() == PHAuthorizationStatus.notDetermined {
+                PHPhotoLibrary.requestAuthorization({(_ status: PHAuthorizationStatus) -> Void in
+                    if status == .authorized {
+                        DispatchQueue.main.async(execute: {
+                            
+                            // Access has been granted.
+                            self.imagePickerController.sourceType = .photoLibrary
+                            self.imagePickerController.delegate = self
+                            UINavigationBar.appearance().titleTextAttributes = [
+                                NSAttributedStringKey.font : UIFont(name: "Ubuntu", size: 16)!,
+                                NSAttributedStringKey.foregroundColor : UIColor.black
+                            ]
+                            self.present(self.imagePickerController, animated: true, completion: {() -> Void in
+                            })
+                        })
+                    }
+                    else {
+                        // Access has been denied.
+                    }
+                })
+            } else {
+                self.alertToEncouragePhotoLibraryAccessWhenApplicationStarts()
+            }
+            
+        })
+        
+        let removeButton = UIAlertAction(title: "Remove", style: .destructive, handler: { (action: UIAlertAction) -> Void in
+            self.removeProfileImage()
+            //backend request to remove image
+        })
+        
+        alert.addAction(cancelButton)
+        alert.addAction(takePhotoButton)
+        alert.addAction(chooseExistingButton)
+        if self.profileImage.image != #imageLiteral(resourceName: "defaultImage")
+        {
+            alert.addAction(removeButton)
+        }
+        self.present(alert, animated: true, completion: {})
+    }
     
+    //removing image from id card
+    func removeProfileImage(){
+        self.profileImage.image = #imageLiteral(resourceName: "defaultImage")
+    }
+    
+    func alertToEncouragePhotoLibraryAccessWhenApplicationStarts()
+    {
+        //Photo Library not available - Alert
+        let cameraUnavailableAlertController = UIAlertController (title: "Photo Library Unavailable", message: "Please check to see if device settings doesn't allow photo library access", preferredStyle: .alert)
+        
+        let settingsAction = UIAlertAction(title: "Settings", style: .destructive) { (_) -> Void in
+            let settingsUrl = URL(string:UIApplicationOpenSettingsURLString)
+            if let url = settingsUrl {
+                UIApplication.shared.open(url, options: [:], completionHandler: nil)
+            }
+        }
+        let cancelAction = UIAlertAction(title: "Okay", style: .default, handler: nil)
+        cameraUnavailableAlertController .addAction(settingsAction)
+        cameraUnavailableAlertController .addAction(cancelAction)
+        self.present(cameraUnavailableAlertController , animated: true, completion: nil)
+    }
+    
+    func alertToEncourageCameraAccessWhenApplicationStarts()
+    {
+        //Camera not available - Alert
+        let internetUnavailableAlertController = UIAlertController (title: "Camera Unavailable", message: "Please check to see if it is disconnected or in use by another application", preferredStyle: .alert)
+        let settingsAction = UIAlertAction(title: "Settings", style: .destructive) { (_) -> Void in
+            let settingsUrl = URL(string:UIApplicationOpenSettingsURLString)
+            if let url = settingsUrl {
+                DispatchQueue.main.async {
+                    UIApplication.shared.open(url, options: [:], completionHandler: nil)
+                }
+            }
+        }
+        let cancelAction = UIAlertAction(title: "Okay", style: .default, handler: nil)
+        internetUnavailableAlertController .addAction(settingsAction)
+        internetUnavailableAlertController .addAction(cancelAction)
+        self.present(internetUnavailableAlertController , animated: true, completion: nil)
+    }
 }
